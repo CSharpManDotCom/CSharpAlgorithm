74136207830c7e2ba7abf70fa206cf785220aa7b
CSharpManDotCom <hanxukun67@gmail.com>
2023-04-03 12:49:55 +0800

    123

---
+++ ACMSGURU/123_TheSum/Program.cs
@@ -0,0 +1,17 @@
+﻿int k = int.Parse(Console.ReadLine()!);
+
+List<int> fib = new();
+fib.Add(1);
+fib.Add(1);
+for (int i = 2; i < 40; i++)
+{
+	fib.Add(fib[i - 2] + fib[i - 1]);
+}
+
+int ans = 0;
+for (int i = 0; i < k; i++)
+{
+	ans += fib[i];
+}
+
+Console.WriteLine(ans);
